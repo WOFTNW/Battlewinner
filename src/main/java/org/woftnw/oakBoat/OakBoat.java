bb104dbdbe69817640e2d529df5f27fed4313a3e
Stonley890 <stonley@macver.org>
2025-02-06 16:47:47 -0800

    Initial commit

---
+++ src/main/java/org/woftnw/oakBoat/OakBoat.java
@@ -0,0 +1,17 @@
+package org.woftnw.oakBoat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OakBoat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
